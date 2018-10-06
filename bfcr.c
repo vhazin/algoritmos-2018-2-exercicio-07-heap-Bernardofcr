519d6c9dd4302bbc47c9e94ca676735a80555e50
bernardo russo <bfcr@cesar.school>
2018-10-06 09:49:55 -0300

    não funciona ainda, inicio da implementação

---
+++ bfcr.c
@@ -0,0 +1,47 @@
+//
+//  main.c
+//  Heap exercicio hazin
+//
+//  Created by Bernardo Russo on 04/10/18.
+//  Copyright © 2018 Bernardo Russo. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct celula{
+    int valor;
+    char ch;
+}cel;
+
+typedef struct Fila{
+    int total;
+    cel *primeiro;
+}fila;
+
+void addcel(){
+    
+}
+
+void addlista(fila *lista, cel c){
+    lista=malloc(sizeof (fila));
+    lista->total=lista->total+1;
+}
+
+
+int main() {
+    int n,i;
+    while (1){
+        scanf ("%d",&n);
+        if (n==0)
+            break;
+        i=0;
+        cel c[n];
+        while (i<n){
+            scanf ("%c/%d",&c[i].ch,&c[i].valor);
+            
+        }
+    }
+    
+    return 0;
+}
